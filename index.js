959c1f0fa1ea71b5425e04965f75577c77405751
LoganLaFollette <LaFollette25@gmail.com>
2020-11-16 17:19:43 -0600

    fixing typo in PREFIX var - whoops

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
-const PREFIX = process.env.PREIX;
+const PREFIX = process.env.PREFIX;
 
 // GCP SETUP
 
