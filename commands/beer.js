d982fca0dcab136ab7c56cbcfce509965a4ace68
LoganLaFollette <LaFollette25@gmail.com>
2020-11-19 20:32:54 -0600

    Beerchart 1.0
    -added initial beerchart implementation - command: !beerchart
    -added sample data used to create chart is generated from (TODO: this should take actual user data users input into firestore/gcp storage)

--- commands/beer.js
+++ commands/beer.js
@@ -1,28 +1,22 @@
 module.exports.run = async (client, msg, args) => {
-    // msg.channel.send('Beer successfully logged!');
-    // this.beerToFind;
-    // if (args[1]) {
-    //     this.beerToFind = args[1];
-    // } else {
-    //     // msg.channel.send('Please provide a beer to search for!');
-    //     return;
-    // }
+    msg.channel.send('Beer successfully logged!');
+    this.beerToFind = args[0];
     
-    // (async () => {
-    //     try {
-    //         const json = await readJsonFromFile('sample-beer-data.json')
-    //         var beerVal = JSON.parse(json).beerlist[this.beerToFind];
-    //         var rating = beerVal.ratings.user1;
-    //         var description = beerVal.descriptions.user1;
-    //         this.msg.channel.send(this.beerToFind + ': (user1) ' + rating + " Description: (user1) " + description);
-    //     } catch (e) {
-    //         console.error(e)
-    //     }
-    // }).bind(this)();
+    (async () => {
+        try {
+            const json = await readJsonFromFile('sample-beer-data.json')
+            var beerVal = JSON.parse(json).beerlist[this.beerToFind];
+            var rating = beerVal.ratings.user1;
+            var description = beerVal.descriptions.user1;
+            this.msg.channel.send(this.beerToFind + ': (user1) ' + rating + " Description: (user1) " + description);
+        } catch (e) {
+            console.error(e)
+        }
+    }).bind(this)();
 
-    // console.log(msg.content);
-    // msg.react('✅');
-    // msg.react('🚫');
+    console.log(msg.content);
+    msg.react('✅');
+    msg.react('🚫');
 };
 
 module.exports.help = {
---
+++ commands/beerchart.js
@@ -0,0 +1,55 @@
+const { CanvasRenderService } = require('chartjs-node-canvas');
+const fs = require('fs');
+
+// sample beer scores
+const getJson = fs.readFileSync(`sample-beer-scores.json`);
+const json = JSON.parse(getJson);
+
+// get our labels and data
+const labels = json.map(function(item) {
+       return item.Beer;
+});    
+const data = json.map(function(item) {
+       return item.Score;
+});
+ 
+const width = 2000; //px
+const height = 2000; //px
+const canvasRenderService = new CanvasRenderService(width, height, (ChartJS) => { });
+
+module.exports.run = async (client, msg, args) => {
+    const configuration = {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Beer Score (out of 10)",
+              backgroundColor: "#3e95cd",
+              data: data
+            }
+          ]
+        },
+        options: {
+          legend: { display: false },
+          title: {
+            display: true,
+            text: 'Moss Beers'
+          }
+        }
+    };
+    const image = await canvasRenderService.renderToBuffer(configuration);
+    const dataUrl = await canvasRenderService.renderToDataURL(configuration);
+    console.log("Chart created! URL: " + dataUrl);
+
+    msg.channel.send('Here is your bar...er uh beerchart!', {
+      files: [
+          image
+      ]
+  });
+
+};
+
+module.exports.help = {
+  name: 'beerchart'
+};
